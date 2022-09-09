68a3eef00955dfb4c83bb079845933bff3259dca
Alan <alancurtis5000+accounts@gmail.com>
2022-09-09 15:20:39 -0600

    added error handling and signup call

--- pages/auth/signup.js
+++ pages/auth/signup.js
@@ -1,13 +1,22 @@
 import { useState } from 'react';
+import axios from 'axios';
 
 export default () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
+    const [errors, setErrors] = useState([])
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
         console.log({ email, password })
-
+        try {
+            const response = await axios.post('/api/users/signup', {
+                email, password
+            })
+            console.log(response.data)
+        } catch (error) {
+            setErrors(error.response.data.errors)
+        }
     }
 
     return (
@@ -25,6 +34,12 @@ export default () => {
                     value={password}
                     onChange={e => setPassword(e.target.value)}></input>
             </div>
+            {errors.length > 0 && <div className='alert alert-danger'>
+                <h4>Ooops...</h4>
+                <ul className='my-0'>
+                    {errors.map(error => <li key={error.message}>{error.message}</li>)}
+                </ul>
+            </div>}
             <button className='btn btn-primary'>Sigh Up</button>
         </form>
     )
