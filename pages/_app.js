564291619982db2c0bccdb3ea8c03bcb0430a450
Alan <alancurtis5000+accounts@gmail.com>
2022-10-07 10:57:10 -0600

    250 app get inital props and page get initial prop

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,32 @@
-import 'bootstrap/dist/css/bootstrap.css'
 // this is how next js applies global styles
+import 'bootstrap/dist/css/bootstrap.css'
+import buildClient from '../api/build-client'
 
-export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+    return (
+        <div>
+            <h1>Header {currentUser.email}</h1>
+            <Component {...pageProps} />
+        </div>
+    )
 }
+
+AppComponent.getInitialProps = async (appContext) => {
+    const client = buildClient(appContext.ctx)
+    const { data } = await client.get('/api/users/currentuser');
+    let pageProps = {};
+    if (appContext.Component.getInitialProps) {
+        pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+    }
+    console.log('app', pageProps, data)
+
+
+    return {
+        pageProps,
+        currentUser: data.currentUser
+    };
+
+
+}
+
+export default AppComponent
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-    console.log(currentUser);
+    console.log('landing', currentUser);
     return currentUser ? <h1>You are signed in</h1> : <h1>You are NOT signed in</h1>;
 };
 
