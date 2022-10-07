248778b34cdac1b126be64cb54379876d61f0193
Alan <alancurtis5000+accounts@gmail.com>
2022-10-07 10:23:30 -0600

    244 client / server api for

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,15 @@
-import axios from 'axios';
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-    // axios.get('/api/users/currentuser');
     console.log(currentUser);
 
     return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-    if (typeof window === 'undefined') {
-        // we are on the server!
-        // requests should be made to http://ingress-nginx.ingress-nginx...laksdjfk
-        // console.log('on server')
-        const { data } = await axios.get(
-            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
-            {
-                headers: req.headers,// passing the headers from the original reqest
-            }
-        );
-
-        return data;
-    } else {
-        // we are on the browser!
-        // requests can be made with a base url of ''
-        // console.log('on client')
-        const { data } = await axios.get('/api/users/currentuser');
-        return data;
-    }
-    return {};
+LandingPage.getInitialProps = async (context) => {
+    const client = buildClient(context)
+    const { data } = await client.get('/api/users/currentuser');
+    return data;
 };
 
-export default LandingPage;
+export default LandingPage; 
