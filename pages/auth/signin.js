0eea09aaebc15b823dd62f8ce3984937268a1490
Alan <alancurtis5000+accounts@gmail.com>
2022-10-07 10:32:24 -0600

    added sign in page

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -6,7 +6,7 @@ export default () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
     const { doRequest, errors } = useRequest({
-        url: '/api/users/signup',
+        url: '/api/users/signin',
         method: 'post',
         body: {
             email,
@@ -22,7 +22,7 @@ export default () => {
 
     return (
         <form onSubmit={onSubmit}>
-            <h1>Sign Up</h1>
+            <h1>Sign In</h1>
             <div className='form-group'>
                 <lable>Email Address</lable>
                 <input className='form-control'
@@ -36,7 +36,7 @@ export default () => {
                     onChange={e => setPassword(e.target.value)}></input>
             </div>
             {errors}
-            <button className='btn btn-primary'>Sigh Up</button>
+            <button className='btn btn-primary'>Sign In</button>
         </form>
     )
 }
