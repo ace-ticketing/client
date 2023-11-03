877da9eb78c8267926055c0f18cd086a81a66ffc
Alan <alancurtis5000+accounts@gmail.com>
2023-11-03 15:00:59 -0600

    Form makes request to tickets api

--- pages/tickets/new.js
+++ pages/tickets/new.js
@@ -1,8 +1,21 @@
 import { useState } from "react";
+import useRequest from "../../hooks/use-request";
 
 const NewTicket = () => {
   const [price, setPrice] = useState("");
   const [title, setTitle] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/tickets",
+    method: "post",
+    body: { title, price },
+
+    onSuccess: (ticket) => console.log(ticket),
+  });
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    doRequest();
+  };
 
   const onBlur = () => {
     const value = parseFloat(price);
@@ -15,7 +28,7 @@ const NewTicket = () => {
   return (
     <div>
       <h1>Create a Ticket</h1>
-      <form>
+      <form onSubmit={onSubmit}>
         <div className="form-group">
           <label>Title</label>
           <input
@@ -33,6 +46,7 @@ const NewTicket = () => {
             className="form-control"
           />
         </div>
+        {errors}
         <button className="btn btn-primary">Submit</button>
       </form>
     </div>
