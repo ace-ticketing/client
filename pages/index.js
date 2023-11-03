221afca126695cbbd272a6007d3043d3a49afdf9
Alan <alancurtis5000+accounts@gmail.com>
2023-11-03 15:11:33 -0600

    display ticket list on landing page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,32 @@
-const LandingPage = ({ currentUser }) => {
-  console.log("landing", currentUser);
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are NOT signed in</h1>
+const LandingPage = ({ currentUser, tickets }) => {
+  const ticketList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <th>{ticket.title}</th>
+        <th>{ticket.price}</th>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>{ticketList}</tbody>
+      </table>
+    </div>
   );
 };
 
 LandingPage.getInitialProps = async (context, client, currentUser) => {
   console.log("hello");
-  return {};
+  const { data } = await client.get("/api/tickets");
+  return { tickets: data };
 };
 
 export default LandingPage;
